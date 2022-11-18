aefb9822909bc0e39acf34c96aafafc9906da192
Xcode-ola <adeolaademilua57@gmail.com>
2022-11-18 13:45:41 +0100

    updated

--- backend/user/views.py
+++ backend/user/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth.models import User
+from .models import User
 from .serializer import RegisterSerializer
 from rest_framework import generics
 
