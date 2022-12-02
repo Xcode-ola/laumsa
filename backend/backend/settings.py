08ca200f1dbb01e13ee44d3b530779c404d794dc
Xcode-ola <adeolaademilua57@gmail.com>
2022-12-02 03:02:19 +0100

    completed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,7 +137,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 SIMPLE_JWT = {
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'), name='main'),
     path('user/', include('user.urls')),
+    path('docs/', include_docs_urls(title='LaumsaQuizAPI')),
+    path('openapi/', get_schema_view(
+        title="LaumsaQuizAPI",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
