a188f9e42f7792b3f00691c935e06065d9366972
Xcode-ola <adeolaademilua57@gmail.com>
2022-12-02 03:07:46 +0100

    completed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ho0@8(nr_7)&4ahpo=%5$qr1cm58age8^uq5xdj=cr(vxew^=z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
